c19f44bde20e25c4675b00757d6b0f7400d42c4a
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2023-08-25 13:59:59 +0100

    Change styling

--- solid-dynamic-image-transform-test/src/routes/index.tsx
+++ solid-dynamic-image-transform-test/src/routes/index.tsx
@@ -2,17 +2,32 @@ import { createSignal, createMemo } from "solid-js";
 import { Title } from "solid-start";
 import Counter from "~/components/Counter";
 import { DynamicImage, Image } from "@solid-mediakit/dynamic-image";
+import { ParentProps } from "solid-js/types/render";
+const Container = (props: ParentProps) => {
+	return <div style={{ height: "200px" }}>{props.children}</div>;
+};
 export default function Home() {
 	const [count, setCount] = createSignal(0);
 	return (
 		<main>
 			<Title>Hello World</Title>
 			<h1>Hello world!</h1>
-			<Image>
-				<DynamicImage>
-					<div>{`This is extremely cool: ${count() * 2}`}</div>
-				</DynamicImage>
-			</Image>
+			<div style={{ display: "flex", "flex-direction": "column" }}>
+				<Container>
+					<Image>
+						<DynamicImage>
+							<div>{`This is extremely cool: ${count() * 2}`}</div>
+						</DynamicImage>
+					</Image>
+				</Container>
+				<Container>
+					<Image>
+						<DynamicImage>
+							<div>{`This is also extremely cool: ${count() * 3}`}</div>
+						</DynamicImage>
+					</Image>
+				</Container>
+			</div>
 			<Counter count={count()} setCount={setCount} />
 			<p>
 				Visit{" "}
